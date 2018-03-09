50c9269311c96b2fcec10e5766d4ffebf745683e
Wei Gao <gaow@seagroup.com>
2018-03-09 16:28:36 +0800

    Add site navigation with styles

--- src/PageHome/index.js
+++ src/PageHome/index.js
@@ -2,10 +2,23 @@ import { h, Component } from 'preact';
 import UpcomingClimbs from './components/UpcomingClimbs';
 import InstagramLink from './components/InstagramLink';
 import Header from './components/Header';
+import Breadcrumb from '../widgets/Breadcrumb';
 import Docked from '../widgets/Docked';
 import './style.scss';
 
 const DockedHeader = Docked(Header);
+const links = [
+  {
+    text: 'climbs',
+    url: '/climbs',
+    active: true,
+  },
+  {
+    text: 'journal',
+    url: '/journal',
+    disabled: true,
+  },
+];
 
 export default class PageHome extends Component {
   render(props) {
@@ -17,6 +30,7 @@ export default class PageHome extends Component {
             <div className="page-home-top-content__header">hello eels</div>
             <div className="page-home-top-content__text">
               eels are a couple of avid rock climbers
+              <Breadcrumb links={links} />
             </div>
             <InstagramLink className="page-home-top-content__instagram-link" />
           </div>
---
+++ src/widgets/Breadcrumb/index.js
@@ -0,0 +1,35 @@
+// @flow
+
+import { h, Component } from 'preact';
+import { Link } from 'preact-router';
+import cx from 'classnames';
+import './style.scss';
+
+type BreadcrumbProps = {
+  links: Link[],
+};
+class Breadcrumb extends Component {
+  render(props: BreadcrumbProps) {
+    const { links = [] } = props;
+
+    return (
+      <div className="eels-breadcrumb">
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            className={cx(
+              'eels-breadcrumb-link',
+              link.active && 'eels-breadcrumb-link--active',
+              link.disabled && 'eels-breadcrumb-link--disabled'
+            )}
+            href={link.url}
+          >
+            <span>{link.text}</span>
+          </Link>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Breadcrumb;
