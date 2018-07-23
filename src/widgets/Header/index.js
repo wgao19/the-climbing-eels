c210482c099efd7632f1968c68c5306debb46de6
Wei Gao <wgao19@gmail.com>
2018-07-24 00:37:28 +0800

    Add mood to header

--- src/widgets/Header/index.js
+++ src/widgets/Header/index.js
@@ -1,20 +1,34 @@
 // @flow
 import React from 'react';
+import cx from 'classnames';
 import { Link } from 'react-router-dom';
 import InstagramLink from '../InstagramLink';
+import Mood from '../Mood';
 import './style.scss';
 
 const Header = () => {
   return (
-    <div className="eels-header">
-      <Link to="/" className="eels-header__main-text">
-        ٩(๑❛ᴗ❛๑)۶
-      </Link>
-      <Link to="/" className="eels-header__secondary-text">
-        us eels ♥ climbing
-      </Link>
-      <InstagramLink className="eels-header__instagram-link" />
-    </div>
+    <Mood.Consumer>
+      {({ mood }) =>
+        <div className="eels-header">
+          <Link to="/" className="eels-header__main-text">
+            ٩(๑❛ᴗ❛๑)۶
+          </Link>
+          <Link to="/" className="eels-header__secondary-text">
+            us eels
+            <span
+              className={cx(
+                'eels-header__heart',
+                mood && `eels-header__heart--${mood}`,
+              )}
+            >
+              ♥
+            </span>
+            climbing
+          </Link>
+          <InstagramLink className="eels-header__instagram-link" />
+        </div>}
+    </Mood.Consumer>
   );
 };
 
