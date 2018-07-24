de6998f342126c82bf13d6c6265d14e03f7329e7
Wei Gao <gaow@seagroup.com>
2018-07-24 15:11:51 +0800

    Add mood to loading spinners

--- src/widgets/Loading/index.js
+++ src/widgets/Loading/index.js
@@ -4,13 +4,19 @@
  * */
 
 import React from 'react';
+import cx from 'classnames';
+import Mood from 'widgets/Mood';
 import './style.scss';
 
 const Loading = () => (
-  <div className="eels-loading">
-    <div className="eels-loading-dot-1" />
-    <div className="eels-loading-dot-2" />
-  </div>
+  <Mood.Consumer>
+    {({ mood }) => (
+      <div className={cx('eels-loading', mood && `eels-loading--${mood}`)}>
+        <div className="eels-loading-dot-1" />
+        <div className="eels-loading-dot-2" />
+      </div>
+    )}
+  </Mood.Consumer>
 );
 
 export default Loading;
